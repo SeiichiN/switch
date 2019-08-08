2f52ed32cf07edc71404eaed0d1e73d2214028b7
Seiichi Nukayama <billie175@gmail.com>
2019-08-08 16:57:32 +0900

    スイッチのオンオフはできた

--- switch.js
+++ switch.js
@@ -3,12 +3,20 @@
 var sw = document.getElementsByClassName('switch');
 var setSw = function () {
   for (var i = 0; i < sw.length; i++) {
-	sw[i].innerHTML = '<img src="switch_off.png" alt="">';
-	sw[i].onclick = (function () {
-	  return function () {console.log(i)};
-	  // sw[i].innerHTML = '<img src="switch_on.png" alt="">';
-	})();
+    sw[i].innerHTML = '<img src="switch_off.png" alt="">';
+	sw[i].onoff = 'off';
+	  
   }
+};
+var changeSw = function (e) {
+	if (e.onoff === 'off') {
+		e.innerHTML = '<img src="switch_on.png" alt="">';
+		e.onoff = 'on';
+	}
+	else {
+		e.innerHTML = '<img src="switch_off.png" alt="">';
+		e.onoff = 'off';
+	}
 };
 window.onload = function () {
   setSw();
