64f89edd33905140d43733ca0db2f6894d10f63f
SeiichiN <billie175@gmail.com>
2022-01-22 20:48:27 +0900

    10進数を表示できるようにした

--- switch.js
+++ switch.js
@@ -5,7 +5,10 @@ var bitno = document.getElementsByClassName('bit');
 var bitArea = document.getElementById('bit-area');
 var hexnoL = document.getElementById('hexL');
 var hexnoR = document.getElementById('hexR');
+var decno = document.getElementById('dec');
+var decArea = document.getElementById('dec-area');
 var showBitBtn = document.getElementById('showBitBtn');
+var showDecBtn = document.getElementById('showDecBtn');
 
 var setSw = function () {
 	for (var i = 0; i < sw.length; i++) {
@@ -14,7 +17,8 @@ var setSw = function () {
 		sw[i].bit = '0';
 		bitno[i].innerHTML = sw[i].bit;
 	}
-	makeHex();
+  makeHex();
+  makeDec();
 };
 var changeSw = function (t) {
 	if (sw[t].onoff === 'off') {
@@ -29,7 +33,8 @@ var changeSw = function (t) {
 		sw[t].bit = '0';
 		bitno[t].innerHTML = sw[t].bit;
 	}
-	makeHex();
+  makeHex();
+  makeDec();
 };
 
 // 2進数の文字列を16進数の文字列に変換する
@@ -50,6 +55,21 @@ var makeHex = function () {
 	hexnoR.innerHTML = strHexR;
 };
 
+var bin2dec = function (sBin) {
+  var number = parseInt(sBin, 2);
+  var sDec = number.toString(10);
+  return sDec;
+}
+
+var makeDec = function () {
+  var strBin = sw[0].bit + sw[1].bit + sw[2].bit + sw[3].bit +
+               sw[4].bit + sw[5].bit + sw[6].bit + sw[7].bit;
+  var strDec = bin2dec(strBin);
+  dec.innerHTML = strDec;
+}
+
+
+
 var setShowBitBtn = function () {
   showBitBtn.innerHTML = '2進数を表示しない';
   showBitBtn.onoff = 'on';
@@ -58,7 +78,7 @@ var setShowBitBtn = function () {
 
 var delBitBtn = function () {
   showBitBtn.innerHTML = '2進数を表示する';
-  var bitArea = document.getElementById('bit-area');
+  // var bitArea = document.getElementById('bit-area');
   bitArea.style.display = 'none';
   showBitBtn.onoff = 'off';
 };
@@ -71,7 +91,31 @@ var toggleBitBtn = function () {
   }
 };
 
+var setShowDecBtn = function () {
+  showDecBtn.innerHTML = '10進数を表示しない';
+  showDecBtn.onoff = 'on';
+  decArea.style.display = 'block';
+};
+
+var delDecBtn = function () {
+  showDecBtn.innerHTML = '10進数を表示する';
+  // var decArea = document.getElementById('dec-area');
+  decArea.style.display = 'none';
+  showDecBtn.onoff = 'off';
+};
+
+var toggleDecBtn = function () {
+  if (showDecBtn.onoff === 'on') {
+	delDecBtn();
+  } else {
+	setShowDecBtn();
+  }
+};
+
 window.onload = function () {
 	setSw();
   setShowBitBtn();
+  setShowDecBtn();
 };
+
+// 修正時刻: Sat Jan 22 20:30:13 2022
